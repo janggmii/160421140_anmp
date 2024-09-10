cb32bf37fbb5bc45c81b057c05a98e8bd212c384
janggmii <janggmii17@gmail.com>
2024-09-10 07:24:38 +0700

    fragment navigation

--- app/src/main/java/com/ubaya/week1anmp/GameFragment.kt
+++ app/src/main/java/com/ubaya/week1anmp/GameFragment.kt
@@ -5,21 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import androidx.navigation.Navigation
+import com.ubaya.week1anmp.databinding.FragmentGameBinding
+import com.ubaya.week1anmp.databinding.FragmentMainBinding
 
 
 class GameFragment : Fragment() {
-
-
-
-
+    private lateinit var  binding: FragmentGameBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBack.setOnClickListener {
+            val action = GameFragmentDirections.actionGameFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 
--- app/src/main/java/com/ubaya/week1anmp/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1anmp/MainActivity.kt
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import com.ubaya.week1anmp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        private lateinit var binding: ActivityMainBinding
+        private lateinit var navController: NavController
 
-    }
 
-    override fun onResume() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            val view = binding.root
+            setContentView(view)
+
+            navController =
+                (supportFragmentManager.findFragmentById(R.id.hostFragment)
+                        as NavHostFragment).navController
+            NavigationUI.setupActionBarWithNavController(this, navController)
+        }
+
+
+        override fun onSupportNavigateUp(): Boolean {
+            return navController.navigateUp()
+        }
+
+
+        override fun onResume() {
         super.onResume()
         Log.d("resume","app is starting")
     }
